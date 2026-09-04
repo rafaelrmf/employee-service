5863b6f0c82821fd82346cea75f12781525fa7cf
Rafael Martínez <mtz19982016@gmail.com>
2026-09-03 21:36:30 -0600

    feat: add employee persistence model

---
+++ src/main/java/com/invex/employeeservice/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.invex.employeeservice.repository;
+
+import com.invex.employeeservice.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
